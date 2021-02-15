690e5957e7ef25b184cefc0dc44c3648caea370d
Krystofer Lando <krystofer02@gmail.com>
2021-02-15 12:24:00 +0100

    initialize project

---
+++ src/main/java/bdstudia/Employee.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bdstudia;
+
+/**
+ *
+ * @author krystofair
+ */
+public class Employee {  
+    private int id;  
+    private String firstName,lastName;  
+
+    public int getId() {  
+        return id;  
+    }  
+    public void setId(int id) {  
+        this.id = id;  
+    }  
+    public String getFirstName() {  
+        return firstName;  
+    }  
+    public void setFirstName(String firstName) {  
+        this.firstName = firstName;  
+    }  
+    public String getLastName() {  
+        return lastName;  
+    }  
+    public void setLastName(String lastName) {  
+        this.lastName = lastName;  
+    }  
+  
+  
+}  
---
+++ src/main/java/bdstudia/Main.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bdstudia;
+
+
+import org.hibernate.Session;    
+import org.hibernate.SessionFactory;    
+import org.hibernate.Transaction;  
+import org.hibernate.boot.Metadata;  
+import org.hibernate.boot.MetadataSources;  
+import org.hibernate.boot.registry.StandardServiceRegistry;  
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;  
+  
+    
+public class Main {    
+public static void main(String[] args) {    
+        
+    //Create typesafe ServiceRegistry object    
+    StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+          
+   Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
+  
+SessionFactory factory = meta.getSessionFactoryBuilder().build();  
+Session session = factory.openSession();  
+Transaction t = session.beginTransaction();   
+            
+    Employee e1=new Employee();    
+    e1.setId(104);    
+    e1.setFirstName("Gaurav");    
+    e1.setLastName("Chawla");    
+        
+    session.save(e1);  
+    t.commit();  
+    System.out.println("successfully saved");    
+    factory.close();  
+    session.close();    
+        
+}    
+}   
